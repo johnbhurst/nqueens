4216e5772fa74a3dcb92abf1d1d9a2899c003a27
John Hurst <john.hurst@redenergy.com.au>
2017-12-17 10:45:44 +1300

    Added new_board(), reordered code.

--- javascript/queens_func.js
+++ javascript/queens_func.js
@@ -2,6 +2,21 @@
 // John Hurst (john.b.hurst@gmail.com)
 // 2017-12-16
 
+function new_board(size) {
+  return {
+    size: size,
+    places: 0,
+    col: 0,
+    diag1: 0,
+    diag2: 0,
+  }
+}
+
+function ok(board, row, col) {
+  return (board.col & (1 << col)) == 0 &&
+         (board.diag1 & (1 << row + col)) == 0 &&
+         (board.diag2 & (1 << row - col + board.size - 1)) == 0
+}
 
 function place(board, row, col) {
   return {
@@ -11,12 +26,6 @@ function place(board, row, col) {
     diag1: board.diag1 | 1 << (row + col),
     diag2: board.diag2 | 1 << (row - col + board.size - 1)
   }
-}
-
-function ok(board, row, col) {
-  return (board.col & (1 << col)) == 0 &&
-         (board.diag1 & (1 << row + col)) == 0 &&
-         (board.diag2 & (1 << row - col + board.size - 1)) == 0
 }
 
 function solve_board(board) {
@@ -35,17 +44,13 @@ function solve_board(board) {
   return result
 }
 
-function solve(size) {
-  return solve_board({size: size, places: 0, col: 0, diag1: 0, diag2: 0})
-}
-
 var start = Number.parseInt(process.argv[2])
 var end = process.argv.length > 3 ?
   Number.parseInt(process.argv[3]) : start
 
 for (var size = start; size <= end; size++) {
   console.time(size);
-  var result = solve(size)
+  var result = solve_board(new_board(size))
   console.timeEnd(size)
   console.log(result)
 }
