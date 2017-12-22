51101120dbaa0917f0099406db79d31006b36dd9
John Hurst <john.hurst@redenergy.com.au>
2017-12-23 08:31:24 +1300

    Made some changes to address pylint issues.

--- python/queens_all_func.py
+++ python/queens_all_func.py
@@ -5,8 +5,9 @@
 import datetime
 import sys
 
-class Board:
-    def __init__(self, size, placed = 0, col = 0, diag1 = 0, diag2 = 0):
+class Board(object):
+    """A puzzle board"""
+    def __init__(self, size, placed=0, col=0, diag1=0, diag2=0):
         self.size = size
         self.placed = placed
         self.col = col
@@ -14,37 +15,40 @@ class Board:
         self.diag2 = diag2
 
     def place(self, row, col):
+        """Place a queen at (row, col)."""
         return Board(
-            self.size, 
-            self.placed + 1, 
+            self.size,
+            self.placed + 1,
             self.col | (1 << col),
             self.diag1 | (1 << row + col),
             self.diag2 | (1 << row - col + self.size - 1)
         )
 
-    def ok(self, row, col):
+    def is_ok(self, row, col):
+        """Return true if position (row, col) is not currently attacked."""
         return (self.col & (1 << col) == 0 and
                 self.diag1 & (1 << row+col) == 0 and
                 self.diag2 & (1 << row-col+self.size-1) == 0)
 
     def solve(self):
+        """Return the number of solutions possible on the board as configured."""
         if self.placed == self.size:
             return 1
         else:
             row = self.placed
             result = 0
             for col in range(self.size):
-                if self.ok(row, col):
+                if self.is_ok(row, col):
                     result += self.place(row, col).solve()
             return result
 
-first = int(sys.argv[1])
-last = int(sys.argv[2])
+FIRST = int(sys.argv[1])
+LAST = int(sys.argv[2])
 
-for size in range(first, last+1):
+for size in range(FIRST, LAST+1):
     start = datetime.datetime.now()
     count = Board(size).solve()
     end = datetime.datetime.now()
     diff = end - start
-    print "Board size {} has {} solutions. Calculated in {} seconds.".format(size, count, diff.total_seconds())
-
+    print "Board size {} has {} solutions. Calculated in {} seconds." \
+      .format(size, count, diff.total_seconds())
