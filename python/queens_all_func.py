a9e67199afbd9631599f28d5f459b2ba58736a11
John Hurst <john.hurst@redenergy.com.au>
2017-12-22 20:47:31 +1100

    Added functional Python version; README.md with timings.

---
+++ python/queens_all_func.py
@@ -0,0 +1,51 @@
+# # Copyright 2017 John Hurst
+# John Hurst (john.b.hurst@gmail.com)
+# 2017-12-22
+
+import datetime
+import sys
+
+class Board:
+    def __init__(self, size, placed = 0, col = 0, diag1 = 0, diag2 = 0):
+        self.size = size
+        self.placed = placed
+        self.col = col
+        self.diag1 = diag1
+        self.diag2 = diag2
+
+    def place(self, row, col):
+        return Board(
+            self.size, 
+            self.placed + 1, 
+            self.col | (1 << col),
+            self.diag1 | (1 << row + col),
+            self.diag2 | (1 << row - col + self.size - 1)
+        )
+
+    def ok(self, row, col):
+        return (self.col & (1 << col) == 0 and
+                self.diag1 & (1 << row+col) == 0 and
+                self.diag2 & (1 << row-col+self.size-1) == 0)
+
+    def solve(self):
+        if self.placed == self.size:
+            return 1
+        else:
+            row = self.placed
+            result = 0
+            for col in range(self.size):
+                if self.ok(row, col):
+                    result += self.place(row, col).solve()
+            return result
+
+first = int(sys.argv[1])
+last = int(sys.argv[2])
+
+for size in range(first, last+1):
+    start = datetime.datetime.now()
+    board = Board(size)
+    count = board.solve()
+    end = datetime.datetime.now()
+    diff = end - start
+    print "Board size {} has {} solutions. Calculated in {} seconds.".format(size, count, diff.total_seconds())
+
