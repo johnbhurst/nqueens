9328ca0c3d7bddc1756d1792c279f5c4fd4af878
John Hurst <john.hurst@redenergy.com.au>
2018-01-08 08:29:49 +1300

    Added beginning GCP scripts.

---
+++ gcp/create_highcpu.sh
@@ -0,0 +1,13 @@
+# Copyright 2018 John Hurst
+# John Hurst (john.b.hurst@gmail.com)
+# 2018-01-08
+
+# n1-highcpu-2 $0.0709/hour
+# n1-highcpu-4 $0.1418/hour
+# n1-highcpu-8 $0.2836/hour
+# n1-highcpu-16 $0.5672/hour
+# n1-highcpu-32 $1.1344/hour
+# n1-highcpu-64 $2.2688/hour
+# n1-highcpu-96 $3.402/hour
+
+
---
+++ gcp/create_project.sh
@@ -0,0 +1,12 @@
+# Copyright 2018 John Hurst
+# John Hurst (john.b.hurst@gmail.com)
+# 2018-01-08
+
+gcloud projects create nqueens-gcp
+
+gcloud beta billing accounts list
+gcloud beta billing projects link nqueens-gcp --billing-account=$BILLING_ACCOUNT
+
+gcloud config set core/project nqueens-gcp
+gcloud config set compute/region australia-southeast1
+
