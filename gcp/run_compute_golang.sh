ee6d9e61675ea6ae1e96aea73f298fa4988d3688
John Hurst <john.hurst@redenergy.com.au>
2018-01-08 09:31:50 +1300

    Added script vars.

--- gcp/run_compute_golang.sh
+++ gcp/run_compute_golang.sh
@@ -11,19 +11,25 @@
 # n1-highcpu-96 $3.402/hour
 
 DIR=`dirname $0`
+PROJECT="nqueens-gcp"
+NAME="queens"
+#ZONE="australia-southeast1-c"
+ZONE="us-central1-a"
+CPUS=64
 
-gcloud compute --project "nqueens-gcp" instances create "queens" \
-  --zone "australia-southeast1-c" \
-  --machine-type "n1-highcpu-16" \
+gcloud compute --project $PROJECT instances create $NAME \
+  --zone $ZONE \
+  --machine-type "n1-highcpu-$CPUS" \
   --image "debian-9-stretch-v20180105" \
   --image-project "debian-cloud" \
   --boot-disk-size "10" \
   --boot-disk-type "pd-standard" \
-  --boot-disk-device-name "queens"
+  --boot-disk-device-name $NAME
 
-gcloud compute scp $DIR/rungo.sh queens: --zone "australia-southeast1-c"
-gcloud compute ssh queens --zone "australia-southeast1-c" --command "sh ./rungo.sh"
+gcloud compute scp $DIR/rungo.sh $NAME: --zone $ZONE
+gcloud compute ssh $NAME --zone $ZONE --command "sh ./rungo.sh"
 
-gcloud -q compute instances --project "nqueens-gcp" delete "queens" \
-  --zone "australia-southeast1-c" \
+gcloud -q compute instances --project $PROJECT delete $NAME \
+  --zone $ZONE \
   --delete-disks=all
+
