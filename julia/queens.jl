f2ac7ea6800b3a5c3dc38a80129caba2d074d061
John Hurst <john.b.hurst@gmail.com>
2018-03-30 11:11:04 +1300

    Restored type information -- oops, spoke too soon. Dynamic typing affects performance.

--- julia/queens.jl
+++ julia/queens.jl
@@ -3,18 +3,18 @@
 # 2018-03-30
 
 struct Board
-  size
-  row
-  cols
-  diags1
-  diags2
+  size::Int
+  row::Int
+  cols::Int
+  diags1::Int
+  diags2::Int
 end
 
 function new(size)
   Board(size, 0, 0, 0, 0)
 end
 
-function place(board, col)
+function place(board::Board, col::Int)
   Board(
     board.size,
     board.row+1,
@@ -24,13 +24,13 @@ function place(board, col)
   )
 end
 
-function ok(board, col)
+function ok(board::Board, col::Int)
   board.cols & (1 << col) |
     board.diags1 & (1 << (board.row + col)) |
     board.diags2 & (1 << (board.row - col + board.size - 1)) == 0
 end
 
-function solve(board)
+function solve(board::Board)
   board.row == board.size ? 1 : begin
     cols = 0:board.size-1
     solutions = map(col -> ok(board, col) ? solve(place(board, col)) : 0, cols)
