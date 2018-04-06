1647d6efb95d6617bf09bed6b0176be046ec05d0
John Hurst <john.b.hurst@gmail.com>
2018-04-07 11:45:02 +1200

    Updated maketree.groovy to transcode SVG to PNG. (GraphViz doesn't work with the SVG images.) Also added CLI option for LR digraph; refinements.

--- doc/search_tree/maketree.groovy
+++ doc/search_tree/maketree.groovy
@@ -2,17 +2,33 @@
 // John Hurst (john.b.hurst@gmail.com)
 // 2018-04-06
 
+@Grab("org.apache.xmlgraphics:batik-transcoder:1.9.1")
+@Grab("org.apache.xmlgraphics:batik-codec:1.9.1")
+import groovy.xml.DOMBuilder
 import groovy.xml.StreamingMarkupBuilder
-import groovy.xml.XmlUtil
+import org.apache.batik.transcoder.image.PNGTranscoder
+import org.apache.batik.transcoder.TranscoderInput
+import org.apache.batik.transcoder.TranscoderOutput
 
 import static java.lang.Integer.parseInt
 
-int size = parseInt(args[0])
+def cli = new CliBuilder(usage: "maketree.groovy [options] size",
+                         header: "Generate tree of N-Queens solutions.\nOptions:")
+cli.h(longOpt: "help", args: 0, "Help")
+cli._(longOpt: "lr", args: 0, "Generate LR (left->right digraph instead of top-bottom")
+
+def options = cli.parse(args)
+if (options.help || !options.arguments()) {
+  cli.usage()
+  System.exit(1)
+}
+
+int size = parseInt(options.arguments()[0])
 Board board = new Board(size: size)
 println """
 digraph tree {
-  ${board.name} [image="${board.name}.svg", label="", shape=none];
-"""
+${options.lr ? "rankdir=LR;" : ""}
+${board.node}"""
 board.save()
 board.solve()
 println "}"
@@ -52,9 +68,8 @@ class Board {
         if (ok(col)) {
           Board next = place(col)
           println """
-          ${next.name} [image="${next.name}.svg", label="", shape=none];
-          ${this.name} -> ${next.name};
-          """
+            ${next.node}
+            ${this.name} -> ${next.name} [style="bold"; arrowsize="2.0"];""".stripIndent()
           next.save()
           result += next.solve()
         }
@@ -67,58 +82,61 @@ class Board {
     return "board_${size}_${positions.join('_')}"
   }
 
-  void save() {
-    File file = new File(name + ".svg")
-    assert !file.exists(), "file $file.name already exists"
+  String getNode() {
+    return """${this.name} [image="${this.name}.png", label="", shape=none];"""
+  }
 
-    def xml = new StreamingMarkupBuilder()
-    def writer = xml.bind {
-      mkp.xmlDeclaration(version: "1.0", encoding: "utf-8")
-      mkp.declareNamespace(xl: "http://www.w3.org/1999/xlink")
-      svg(version: "1.1", xmlns: "http://www.w3.org/2000/svg", viewBox: "-5 -5 ${size*100+10} ${size*100+10}", width: size*100, height: size*100) {
+  void save() {
+    def builder = new StreamingMarkupBuilder()
+    def xml = builder.bind {
+      namespaces << ["": "http://www.w3.org/2000/svg", "xl": "http://www.w3.org/1999/xlink"]
+      svg(version: "1.1", viewBox: "-5 -5 ${size*100+10} ${size*100+10}", width: "${size*100}", height: "${size*100}") {
         def svg = delegate
         defs {
-          style(type: "text/css") {
-            mkp.yieldUnescaped("""<![CDATA[
+          style(type: "text/css", """
             .piece { font-size: 90px; font-weight: 400; }
             .blackp { fill: black; }
             .whitep { fill: white; stroke: black; stroke-width: 2px;}
-          ]]>""")
-          }
-          rect(id: "whitesquare", width: 100, height: 100, stroke: "black", "stroke-width": 3, fill: "#ddd")
-          rect(id: "blacksquare", width: 100, height: 100, stroke: "black", "stroke-width": 3, fill: "#666")
+          """)
+          rect(id: "whitesquare", width: "100", height: "100", stroke: "black", "stroke-width": "3", fill: "#fff")
+          rect(id: "blacksquare", width: "100", height: "100", stroke: "black", "stroke-width": "3", fill: "#ccc")
           g(id: "oddrow") {
             for (col in 1..size) {
-              svg.use("xl:href": (col%2==0) ? "#blacksquare" : "#whitesquare", x: (col-1)*100, y: 0)
+              svg.use("xl:href": (col%2==0) ? "#blacksquare" : "#whitesquare", x: "${(col-1)*100}", y: "0")
             }
           }
           g(id: "evenrow") {
             for (col in 1..size) {
-              svg.use("xl:href": (col%2==0) ? "#whitesquare" : "#blacksquare", x: (col-1)*100, y: 0)
+              svg.use("xl:href": (col%2==0) ? "#whitesquare" : "#blacksquare", x: "${(col-1)*100}", y: "0")
             }
           }
           g(id: "board") {
             for (row in 1..size) {
-              svg.use("xl:href": (row%2==0) ? "#evenrow" : "#oddrow", x: 0, y: (row-1)*100)
+              svg.use("xl:href": (row%2==0) ? "#evenrow" : "#oddrow", x: "0", y: "${(row-1)*100}")
             }
-            rect(x: 0, y: 0, width: size*100, height:size*100, stroke: "black", "stroke-linecap": "round", "stroke-linejoin": "round", "stroke-width": 8, fill: "none")
+            rect(x: "0", y: "0", width: "${size*100}", height: "${size*100}", stroke: "black", "stroke-linecap": "round", "stroke-linejoin": "round", "stroke-width": "8", fill: "none")
           }
           text(id: "blackqueen") {
-            tspan(class: "blackp piece", x: "5", y: "80") {
-              mkp.yieldUnescaped("&#x265B;")
-            }
+            tspan(class: "blackp piece", x: "5", y: "80", "♛")
           }
         }
         g {
           svg.use("xl:href": "#board", x: "0", y: "0")
           positions.eachWithIndex {col, i ->
-            svg.use("xl:href": "#blackqueen", x: (col-1)*100, y: i*100)
+            svg.use("xl:href": "#blackqueen", x: "${(col-1)*100}", y: "${i*100}")
           }
         }
       }
     }
-
-    file.text = XmlUtil.serialize(writer)
+    def doc = DOMBuilder.parse(new StringReader(xml.toString()), false, true)
+    File file = new File(name + ".png")
+    assert !file.exists(), "file $file.name already exists"
+    PNGTranscoder transcoder = new PNGTranscoder()
+    TranscoderInput input = new TranscoderInput(doc)
+    file.withOutputStream {os ->
+      TranscoderOutput output = new TranscoderOutput(os)
+      transcoder.transcode(input, output)
+    }
   }
 }
 
