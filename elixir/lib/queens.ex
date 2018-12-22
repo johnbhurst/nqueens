d94cf08bab91b426dce4a3a3c59a8843958a4501
John Hurst <john.b.hurst@gmail.com>
2018-12-22 14:31:41 +1300

    feat: First working Elixir implementation.

--- elixir/lib/queens.ex
+++ elixir/lib/queens.ex
@@ -1,5 +1,61 @@
+defmodule Board do
+  defstruct size: 8, row: 0, cols: 0, diags1: 0, diags2: 0
+end
+
+
 defmodule Queens do
+  use Bitwise
+
+  def ok(board, col) do
+    (board.cols &&& (1 <<< col)) == 0 and
+    (board.diags1 &&& (1 <<< board.row + col)) == 0 and
+    (board.diags2 &&& (1 <<< board.row - col + board.size - 1)) == 0
+  end
+
+  def place(board, col) do
+    %Board{
+      size: board.size,
+      row: board.row + 1,
+      cols: board.cols ||| (1 <<< col),
+      diags1: board.diags1 ||| (1 <<< board.row + col),
+      diags2: board.diags2 ||| (1 <<< board.row - col + board.size - 1)
+    }
+  end
+
+  def solve(board) do
+    if board.row == board.size do
+      1
+    else
+      Enum.sum(
+        Enum.map(
+          Enum.filter(
+            1..board.size,
+            fn col -> ok(board, col) end
+          ),
+          fn col -> solve(place(board, col)) end
+        )
+      )
+    end
+  end
+
   def main(argv) do
-    IO.puts "Hello World (#{argv})"
+    from = if length(argv) > 0 do
+      String.to_integer(Enum.at(argv, 0))
+    else
+      8
+    end
+    to = if length(argv) > 1 do
+      String.to_integer(Enum.at(argv, 1))
+    else
+      from
+    end
+
+    for size <- from..to do
+      start = Time.utc_now
+      result = solve(%Board{size: size})
+      duration = Time.diff(Time.utc_now, start, 1000)/1000
+      IO.puts "#{size},#{result},#{duration}"
+    end
+
   end
 end
