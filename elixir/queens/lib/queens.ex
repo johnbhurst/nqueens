db1563dd7ac809c906b14e73da7b5b1eb9776cab
John Hurst <john.b.hurst@gmail.com>
2018-12-22 13:33:10 +1300

    feat: Added initial Elixir module created with 'mix new queens'.

---
+++ elixir/queens/lib/queens.ex
@@ -0,0 +1,18 @@
+defmodule Queens do
+  @moduledoc """
+  Documentation for Queens.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Queens.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/queens/test/queens_test.exs
@@ -0,0 +1,8 @@
+defmodule QueensTest do
+  use ExUnit.Case
+  doctest Queens
+
+  test "greets the world" do
+    assert Queens.hello() == :world
+  end
+end
