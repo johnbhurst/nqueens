5f6cdef75510974bce385ec1cd2695325ba1c801
John Hurst <john.b.hurst@gmail.com>
2018-12-24 09:54:48 +1300

    improvement: Standard run script; standard output format.

--- crystal/queens.cr
+++ crystal/queens.cr
@@ -3,7 +3,6 @@
 # 2018-12-22
 
 class Board
-
   @size : Int32
   @row : Int32
   @cols : Int32
@@ -44,6 +43,6 @@ to = ARGV ? ARGV.shift.to_i : from
 from.upto(to) do |size|
   start = Time.now
   solutions = Board.new(size).solve
-  duration = Time.now - start
-  puts "Board size #{size} has #{solutions} solutions. Calculated in #{duration} seconds."
+  duration = sprintf("%0.3f", (Time.now - start).to_f)
+  puts "#{size},#{solutions},#{duration}"
 end
