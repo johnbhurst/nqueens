4e36e37bc7f9bdae6a183c5f8f64158fbebcf3fe
John Hurst <john.b.hurst@gmail.com>
2018-12-25 11:14:18 +1300

    improvement: Standard run script.

---
+++ elixir/run.sh
@@ -0,0 +1,12 @@
+# Copyright 2018 John Hurst
+# John Hurst (john.b.hurst@gmail.com)
+# 2018-12-25
+
+DIR=$(dirname $0)
+NAME=$1
+FROM=$2
+TO=$3
+
+cd $DIR
+mix escript.build >/dev/null
+./$NAME $FROM $TO
