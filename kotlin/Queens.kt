9a99077d0ba0eb3df64fcd51097452e6d9f94cc3
John Hurst <john.b.hurst@gmail.com>
2018-12-26 14:28:40 +1300

    improvement: Removed unnecessary 'time' method; reordered members.

--- kotlin/Queens.kt
+++ kotlin/Queens.kt
@@ -8,15 +8,6 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit.NANOS
 import java.time.temporal.ChronoUnit.SECONDS
 
-fun <T> time(f: () -> T): Unit {
-  val start = Instant.now()
-  val result = f()
-  val end = Instant.now()
-  val duration = Duration.between(start, end)
-  val seconds = duration.get(SECONDS) + duration.get(NANOS)/1000000000.0
-  println("$result,$seconds")
-}
-
 class Board(
   val size: Int = 0,   // size of board
   val row: Int = 0,    // current row placed
@@ -24,6 +15,11 @@ class Board(
   val diags1: Int = 0, // bits indicating occupied diagonal
   val diags2: Int = 0  // bits indicating occupied diagonal
 ) {
+  fun ok(col: Int) =
+    (cols and (1 shl col)) or
+    (diags1 and (1 shl row + col)) or
+    (diags2 and (1 shl row - col + size - 1)) == 0
+
   fun place(col: Int) = Board(
       size = this.size,
       row = this.row + 1,
@@ -32,24 +28,23 @@ class Board(
       diags2 = this.diags2 or (1 shl this.row - col + this.size - 1)
     )
 
-  fun ok(col: Int) =
-    (cols and (1 shl col)) or
-    (diags1 and (1 shl row + col)) or
-    (diags2 and (1 shl row - col + size - 1)) == 0
-
   fun solve(): Int =
     if (row == size) 1 else
       (0 until size)
         .filter(::ok)
         .map { place(it).solve() }
         .sum()
-
 }
 
 fun main(args: Array<String>) {
   val from = if (args.size > 0) Integer.parseInt(args[0]) else 8
   val to = if (args.size > 1) Integer.parseInt(args[1]) else from
   for (size in from..to) {
-    time {"$size,${Board(size).solve()}"}
+    val start = Instant.now()
+    val result = Board(size).solve()
+    val end = Instant.now()
+    val duration = Duration.between(start, end)
+    val seconds = duration.get(SECONDS) + duration.get(NANOS)/1000000000.0
+    println("$size,$result,$seconds")
   }
 }
