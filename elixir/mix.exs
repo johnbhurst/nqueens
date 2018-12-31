0f903e6c5f97b216ce9c7c40a5eb4e305ab9ef37
John Hurst <john.b.hurst@gmail.com>
2019-01-01 07:54:21 +1300

    improvement: Changed all default implementations to the same name: 'Queens'.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -3,7 +3,7 @@ defmodule Queens.MixProject do
 
   def project do
     [
-      app: :queens,
+      app: :Queens,
       version: "0.1.0",
       elixir: "~> 1.7",
       escript: escript_config(),
