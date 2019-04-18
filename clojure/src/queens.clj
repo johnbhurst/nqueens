81f57dabd8e7d7cd9ff5121194739c4e21865c09
Nick Jones <nick.jones@arachnid.co.nz>
2019-04-18 20:57:38 +1200

    improvement: Introduced type hints and record to improve performance.

--- clojure/src/queens.clj
+++ clojure/src/queens.clj
@@ -2,77 +2,57 @@
 ; John Hurst (john.b.hurst@gmail.com)
 ; 2018-12-31 (Happy New Year!)
 
-(ns queens (:gen-class))
-;(ns queens)
+(ns queens
+  (:gen-class))
 
-(defn newBoard [size]
-  {:size size, :row 0, :cols 0, :diags1 0, :diags2 0}
-)
+(defrecord Board [^Long size ^Long row ^Long cols ^Long diags1 ^Long diags2])
 
-(defn ok [board col]
-  (let [
-      size (:size board)
-      row (:row board)
-      cols (:cols board)
-      diags1 (:diags1 board)
-      diags2 (:diags2 board)
-    ] (and
-      (= (bit-and cols (bit-shift-left 1 col)) 0)
-      (= (bit-and diags1 (bit-shift-left 1 (+ row col))) 0)
-      (= (bit-and diags2 (bit-shift-left 1 (- (+ row size) col 1))) 0)
-    )
-  )
-)
+(defn new-board [size]
+  (->Board size 0 0 0 0))
 
-(defn place [board col]
-  (let [
-      size (:size board)
-      row (:row board)
-      cols (:cols board)
-      diags1 (:diags1 board)
-      diags2 (:diags2 board)
-    ] {
-      :size size,
-      :row (+ row 1),
-      :cols (bit-or cols (bit-shift-left 1 col)),
-      :diags1 (bit-or diags1 (bit-shift-left 1 (+ row col))),
-      :diags2 (bit-or diags2 (bit-shift-left 1 (- (+ row size) col 1)))
-    }
-  )
-)
+(defn ok [board ^Long col]
+  (let [^Long size   (:size board)
+        ^Long row    (:row board)
+        ^Long cols   (:cols board)
+        ^Long diags1 (:diags1 board)
+        ^Long diags2 (:diags2 board)] 
+      (and
+        (= (bit-and cols   (bit-shift-left 1 col)) 0)
+        (= (bit-and diags1 (bit-shift-left 1 (+ row col))) 0)
+        (= (bit-and diags2 (bit-shift-left 1 (- (+ row size) col 1))) 0))))
+
+(defn place [board ^Long col]
+  (let [^Long size   (:size board)
+        ^Long row    (:row board)
+        ^Long cols   (:cols board)
+        ^Long diags1 (:diags1 board)
+        ^Long diags2 (:diags2 board)] 
+        (->Board 
+          size
+          (+ row 1)
+          (bit-or cols (bit-shift-left 1 col))
+          (bit-or diags1 (bit-shift-left 1 (+ row col)))
+          (bit-or diags2 (bit-shift-left 1 (- (+ row size) col 1))))))
 
 (defn solve [board]
-  (let [
-      size (:size board)
-      row (:row board)
-    ]
+  (let [^Long size (:size board)
+        ^Long row  (:row board)]
     (if
       (= row size) 1
-      (apply +
+      (reduce +
         (map (fn [col] (solve (place board col)))
-          (filter (fn [col] (ok board col)) (range size))
-        )
-      )
-    )
-  )
-)
+          (filter (fn [col] (ok board col)) (range size)))))))
 
-(defn run [size]
-  (let [
-    start (java.time.Instant/now)
-    result (solve (newBoard size))
-    end (java.time.Instant/now)
-    duration (java.time.Duration/between start end)
-    seconds (+ (.get duration java.time.temporal.ChronoUnit/SECONDS)
-               (/ (.get duration java.time.temporal.ChronoUnit/NANOS) 1000000000.0))
-  ]
-    (str size "," result "," seconds)
-  )
-)
+(defn run [^Long size]
+  (let [start (java.time.Instant/now)
+        result (solve (new-board size))
+        end (java.time.Instant/now)
+        duration (java.time.Duration/between start end)
+        seconds (+ (.get duration java.time.temporal.ChronoUnit/SECONDS)
+                   (/ (.get duration java.time.temporal.ChronoUnit/NANOS) 1000000000.0))]
+    (str size "," result "," seconds)))
 
 (defn -main
   [from to]
   (doseq [size (range (Integer/parseInt from) (+ (Integer/parseInt to) 1))]
-    (println (run size))
-  )
-)
+    (println (run size))))
