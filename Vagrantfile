d02dfd1b380a84b56a10f1ca43a7a13a0c9dada7
John Hurst <john.b.hurst@gmail.com>
2020-04-14 07:42:14 +1200

    feat: Got Vagrant/Ansible working with Google Compute Engine.

--- Vagrantfile
+++ Vagrantfile
@@ -2,13 +2,38 @@
 # John Hurst (john.b.hurst@gmail.com)
 # 2019-06-14
 
+# To run with default VirtualBox provider, just:
+#   vagrant up
+# To run with Google Cloud Platform:
+#   VAGRANT_DEFAULT_PROVIDER=google vagrant up
+
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/bionic64"
-  config.ssh.insert_key = false
-  config.vm.provision "ansible" do |ansible|
-    ansible.compatibility_mode = "2.0"
-    ansible.host_key_checking = false
+
+  config.vm.provider :google do |google, override|
+    google_project_id = ENV["GOOGLE_PROJECT_ID"] || "nqueens-gcp"
+    google_json_key_location = ENV["GOOGLE_JSON_KEY_LOCATION"] || "~/.config/keys/#{google_project_id}-vagrant.json"
+    google_private_key_path = ENV["GOOGLE_PRIVATE_KEY_PATH"] || "~/.ssh/#{google_project_id}_rsa"
+
+    google.google_project_id = google_project_id
+    google.google_json_key_location = google_json_key_location
+    google.image_project_id = "ubuntu-os-cloud"
+    google.image_family = "ubuntu-1804-lts"
+
+    override.ssh.username = ENV["USER"]
+    override.ssh.private_key_path = google_private_key_path
+    override.vm.box = "google/gce"
+    override.vm.synced_folder "./", "/vagrant", type: "rsync", rsync__exclude: [
+      '.git', 'ansible', 'archive', 'clojure/target', 'csharp/bin', 'csharp/obj',
+      'doc', 'fsharp/bin', 'fsharp/obj', 'nim/nimcache', 'swift/.build', 'tmp', 'venv'
+    ]
+  end
+
+  config.vm.provision :ansible do |ansible|
     ansible.playbook = "ansible/playbook.yml"
-    ansible.verbose = "v"
+    ansible.compatibility_mode = "2.0"
+    ansible.extra_vars = {
+      local_download: false # ENV["VAGRANT_DEFAULT_PROVIDER"] == "google"
+    }
   end
 end
--- gcloud/create_project.sh
+++ gcloud/create_project.sh
@@ -2,12 +2,31 @@
 # John Hurst (john.b.hurst@gmail.com)
 # 2018-01-08
 
-gcloud projects create nqueens-gcp
+# Set this variable to a unique name for your Google Cloud Project. (You cannot use "nqueens-gcp", because it is already used.)
+# export GOOGLE_PROJECT_ID=nqueens-ncp
+# Set your GCP billing account - you have to pay for GCE VMs.
+# export BILLING_ACCOUNT={my billing account}
+
+# Create project
+gcloud projects create $GOOGLE_PROJECT_ID
 
 gcloud beta billing accounts list
-gcloud beta billing projects link nqueens-gcp --billing-account=$BILLING_ACCOUNT
+gcloud beta billing projects link $GOOGLE_PROJECT_ID --billing-account=$BILLING_ACCOUNT
 
-gcloud config set core/project nqueens-gcp
+gcloud config set core/project $GOOGLE_PROJECT_ID
 gcloud config set compute/region australia-southeast1
+gcloud config set compute/zone australia-southeast1-a
 
 gcloud services enable cloudfunctions.googleapis.com
+
+# Create service account and key
+gcloud --project=$GOOGLE_PROJECT_ID iam service-accounts create vagrant --display-name="Service account for Vagrant provisioning"
+gcloud projects add-iam-policy-binding $GOOGLE_PROJECT_ID --member serviceAccount:vagrant@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com --role roles/compute.admin
+
+gcloud iam service-accounts keys create ~/.config/keys/${GOOGLE_PROJECT_ID}-vagrant.json --iam-account vagrant@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
+
+# Create SSH key a configure it in Google Compute Engine
+ssh-keygen -t rsa -f ~/.ssh/${GOOGLE_PROJECT_ID}_rsa -C $USER
+cat ~/.ssh/${GOOGLE_PROJECT_ID}_rsa.pub | sed -e "s/^/${USER}:/" > ssh-keys
+gcloud compute project-info add-metadata --metadata-from-file ssh-keys=ssh-keys
+rm ssh-keys
